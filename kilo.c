3397c7312026228c8d8edcd0a6348433e287230c
MimiMagusin <mimi_magusin@hotmail.com>
2018-09-28 16:12:40 +0200

    Add welcome message

--- kilo.c
+++ kilo.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 // Defines
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k)&0x1f)
 
 // Data
@@ -116,7 +117,21 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\r\n", 2);
     if (y < E.screenrows -1) {
